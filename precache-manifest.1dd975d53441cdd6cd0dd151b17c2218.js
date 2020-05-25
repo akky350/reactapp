9c4c76435f863c540d97118672835005b339e397
akash kumar <akashk.kumar8@gmail.com>
2020-05-25 14:55:24 +0530

    Updates

---
+++ precache-manifest.1dd975d53441cdd6cd0dd151b17c2218.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dad619e33293c3e1a9f3c978a9eca33",
+    "url": "/akky350/reactapp/index.html"
+  },
+  {
+    "revision": "6b505e05a6a074e75382",
+    "url": "/akky350/reactapp/static/css/2.3e87256a.chunk.css"
+  },
+  {
+    "revision": "be0bf295cd2a2b5e3805",
+    "url": "/akky350/reactapp/static/css/main.e74cda0e.chunk.css"
+  },
+  {
+    "revision": "6b505e05a6a074e75382",
+    "url": "/akky350/reactapp/static/js/2.35d55538.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/akky350/reactapp/static/js/2.35d55538.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be0bf295cd2a2b5e3805",
+    "url": "/akky350/reactapp/static/js/main.9861e4ce.chunk.js"
+  },
+  {
+    "revision": "5f9b2e627bbcc2108220",
+    "url": "/akky350/reactapp/static/js/runtime-main.8f393971.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/akky350/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
